cfbd582268a28f76fc5b7228b5ea59219c0e8a6c
Rajkalash Rajbhar <rajkalashrajbhar@gmail.com>
2021-12-07 10:38:19 +0530

    matrix traversal

--- 2DArray/TwoDArray.java
+++ 2DArray/TwoDArray.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 public class TwoDArray {
         public static Scanner sc=new Scanner(System.in);
-    
-        public static void main(String [] args)
-        {            
+        public static void printMultiplyMatrix()
+        {
             int m1=sc.nextInt();
             int n1=sc.nextInt();
             int [][] A=new int[m1][n1];
@@ -23,6 +22,180 @@ public class TwoDArray {
             }
             //display(arr);
             //multipyMatrix(arr);
+        }    
+        public static void main(String [] args)
+        {    
+            //printSpiral();        
+            printSpiral2();
+//            printStateOfWakanda();
+           // printStateOfWanakand2();
+        }
+        public static void printSpiral2()
+        {
+            int m=sc.nextInt();
+            int n=sc.nextInt();
+            int [][] arr=new int[m][n];
+            input(arr);
+            spiral2(arr);
+        }
+        public static void spiral2(int [][] arr)
+        {
+            int m=arr.length;
+            int n=arr[0].length;
+            int count=0;
+            int total=m*n;
+            int minr=0;
+            int minc=0;
+            int maxr=m-1;
+            int maxc=n-1;
+            while(count<total)
+            {
+                for(int i=minc;i<=maxc && count<total;i++)
+                {
+                    System.out.println(arr[minr][i]);
+                    count++;
+                }
+                minr++;
+                for(int i=minr;i<=maxr && count<total;i++ )
+                {
+                    System.out.println(arr[i][maxc]);
+                    count++;
+                }
+                maxc--;
+                for(int i=maxc;i>=minc && count<total;i--)
+                {
+                    System.out.println(arr[maxr][i]);
+                    count++;
+                }
+                maxr--;
+                for(int i=maxr;i>=minr && count<total;i--)
+                {
+                    System.out.println(arr[i][minc]);
+                    count++;
+                }
+                minc++;
+            }
+        }
+        public static void printSpiral()
+        {
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            int  [][] arr=new int[n][m];
+            input(arr);
+            spiral(arr);
+            
+        }
+        // public static void inputCharArray(char [][] arr)
+        // {
+        //     int n=arr.length;
+        //     int m=arr[0].length;
+        //     for(int i=0;i<n;i++)
+        //     {
+        //         for(int j=0;j<m;j++)
+        //         {
+        //             arr[i][j]=sc.next
+        //         }
+        //     }
+        // }
+        public static void spiral(int [][] arr)
+        {
+            int m=arr.length;
+            int n= arr[0].length;
+            int count=0;
+            int total=m*n;
+            int minr=0;
+            int minc=0;
+            int maxr=m-1;
+            int maxc=n-1;
+            while(count<total)
+            {
+                for(int i=minr;i<=maxr && count<total;i++)
+                {
+                    System.out.println(arr[i][minc]);
+                    count++;
+                }
+                minc++;
+                for(int i=minc;i<=maxc && count<total;i++)
+                {
+                    System.out.println(arr[maxr][i]);
+                    count++;
+                }
+                maxr--;
+                for(int i=maxr;i>=minr && count<total;i--)
+                {
+                    System.err.println(arr[i][maxc]);
+                    count++;
+                }
+                maxc--;
+                for(int i=maxc;i>=minc && count<total;i--)
+                {
+                    System.out.println(arr[minr][i]);
+                    count++;
+                }
+                minr++;
+
+            }
+        }
+        public static void printStateOfWanakand2()
+        {
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            int [][] arr=new int[n][m];
+            input(arr);
+            stateOfWakanda2(arr);
+
+        }
+        public static void stateOfWakanda2(int [][] arr)
+        {
+            int n=arr.length;
+            int m=arr[0].length;
+            for(int c=0;c<n;c++)
+            {
+                if(c%2==0)
+                {
+                    for(int r=m-1;r>=0;r--)
+                    {
+                        System.out.println(arr[r][c]);
+                    }
+                }
+                else{
+                    for(int r=0;r<m;r++)
+                    {
+                        System.out.println(arr[r][c]);
+                    }
+                }
+            }
+        }
+        public static void printStateOfWakanda()
+        {
+            int m=sc.nextInt();
+            int n=sc.nextInt();
+            int [][] arr=new int[m][n];
+            input(arr);
+            stateOfWakanda(arr);
+        }
+        public static void stateOfWakanda(int [][] arr)
+        {
+            int m=arr.length;
+            int n=arr[0].length;
+            for(int c=0;c<n;c++)
+            {
+                if(c%2==0)
+                {
+                    for(int r=0;r<m;r++)
+                    {
+                        System.out.println(arr[r][c]);
+                    }
+
+                }
+                else{
+                    for(int r=m-1;r>=0;r--)
+                    {
+                        System.out.println(arr[r][c]);
+                    }
+                }
+   
+            }
         }
         public static void input(int [][] arr)
         {
--- Array/ArrayQuestions.java
+++ Array/ArrayQuestions.java
@@ -1,5 +1,28 @@
 import java.util.Scanner;
+
+//import javax.lang.model.util.ElementScanner6;
 public class ArrayQuestions{
+    public static Scanner scn=new Scanner(System.in);
+    public static int binarySearch(int [] arr,int x)
+    {
+        int li=0,ri=arr.length-1;
+        while(li<=ri)
+        {
+            int mid=(li+ri)/2;
+            if(x==arr[mid]){
+                return mid;
+            }
+            else if(x<arr[mid])
+            {
+                ri=mid-1;
+            }
+            else if(x>arr[mid])//default condition
+            {
+                li=mid+1;
+            }
+        }
+        return -1;
+    }
     public static void subset(int [] arr)
     {   int n=arr.length;
         int total=(int)Math.pow(2,n);
@@ -15,7 +38,7 @@ public class ArrayQuestions{
                     System.out.print("-\t");
                 }
                 else{
-                    System.out.print(arr[j]+"\t");
+                    System.out.println(arr[j]+"\t");
                 }
               //  if((i & (1<<j))==0)
                 //{
@@ -28,15 +51,139 @@ public class ArrayQuestions{
         }
 
     }
+    public static void printCeilAndFloor()
+    {
+        int n=scn.nextInt();
+        int [] arr=new int[n];
+        inputOneDArray(arr);
+        int x=scn.nextInt();
+        CeilAndFloor(arr,x);   
+    }
+    public static void CeilAndFloor(int [] arr,int x)
+    {
+        int li=0,ri=arr.length-1;
+        int ceil=-1,floor=-1;
+        while(li<=ri){
+            int mid=(li+ri)/2;
+            if(x==arr[mid])
+            {
+                floor=arr[mid];
+                ceil=arr[mid];
+                break;
+            }
+            else if(x<arr[mid])
+            {
+                ceil=arr[mid];
+                ri=mid-1;
+            }
+            else{
+                li=mid+1;
+                floor=arr[mid];
+            }
+        }
+        System.out.println("Floor: "+floor);
+        System.out.println("Ceil: "+ceil);
+    }
+    public static void inputOneDArray(int [] arr)
+    {
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=scn.nextInt();
+        }
+    }
+    public static void printFirstAndLastIndex()
+    {
+        int n=scn.nextInt();
+        int [] arr=new int[n];
+        inputOneDArray(arr);
+        int x=scn.nextInt();
+        int fi=firstIndex(arr,x);
+        int li=lastIndex(arr,x);
+        System.out.println("First index: "+fi);
+        System.out.println("last index: "+li);
+    }
+    public static int lastIndex(int [] arr,int x)
+    {
+        int lsti=-1;
+        int li=0,ri=arr.length-1;
+        while(li<=ri){
+            int mid=(li+ri)/2;
+            if(arr[mid]==x)
+            {
+                lsti=mid;
+                li=mid+1;
+            }
+            else if(x<arr[mid])
+            {
+                ri=mid-1;
+            }
+            else{
+                li=mid+1;
+            }
+        }
+        return lsti;
+    }
+    public static int firstIndex(int [] arr,int x)
+    {
+        int fi=-1;
+        int li=0,ri=arr.length-1;
+        while(li<=ri){
+            int mid=(li+ri)/2;
+            if(x==arr[mid])
+            {
+                fi=mid;
+                ri=mid-1;
+            }
+            else if(x<arr[mid])
+            {
+                ri=mid-1;
+            }
+            else{
+                li=mid+1;
+            }
+        }
+        return fi;
+    }
+    public static void printSubArray()
+    {
+        int n=scn.nextInt();
+        int [] arr=new int[n];
+        inputOneDArray(arr);
+        subArray(arr);
+
+    }
+    public static void subArray(int []arr)
+    {
+        int n=arr.length;
+        for(int start=0;start<n;start++)
+        {
+            for(int end=start;end<n;end++)
+            {
+                for(int k=start;k<=end;k++)
+                {
+                    System.err.print(arr[k]+"\t");
+                }
+                System.err.println();
+            }
+        }
+    }
     public static void main(String [] args){
-    Scanner scn=new Scanner(System.in);
-    int n=scn.nextInt();
+        printSubArray();
+        //printFirstAndLastIndex();
+
+    //    printCeilAndFloor();
+  //  Scanner scn=new Scanner(System.in);
+    /*int n=scn.nextInt();
     int [] arr=new int[n];
     for(int i=0;i<n;i++)
     {
         arr[i]=scn.nextInt();
     }
-    subset(arr);
+    int x=scn.nextInt();
+    int ans=binarySearch(arr, x);
+    System.out.println("Ans: "+ans);*/
+    //subset(arr);
 //    int k=scn.nextInt();
   //  rotateByK(arr,k);
     //display(arr);
